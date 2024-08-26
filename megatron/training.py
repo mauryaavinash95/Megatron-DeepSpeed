cca2b0f492b7f6da758efb82b9edd7dd74a0e8c6
YiSheng5 <syhm@mail.ustc.edu.cn>
2024-08-26 23:49:19 +0800

    enable empty cache on XPU device (#438)

--- megatron/training.py
+++ megatron/training.py
@@ -734,7 +734,7 @@ def train_step(forward_step_func, data_iterator,
 
     # Empty unused memory.
     if args.empty_unused_memory_level >= 1:
-        torch.cuda.empty_cache()
+        get_accelerator().empty_cache()
 
     # Reduce gradients.
     if not args.deepspeed:
@@ -796,7 +796,7 @@ def train_step(forward_step_func, data_iterator,
 
         # Empty unused memory.
         if args.empty_unused_memory_level >= 2:
-            torch.cuda.empty_cache()
+            get_accelerator().empty_cache()
 
         if mpu.is_pipeline_last_stage(ignore_virtual=True):
             # Average loss across microbatches.
@@ -1464,7 +1464,7 @@ def evaluate(forward_step_func,
 
             # Empty unused memory
             if args.empty_unused_memory_level >= 1:
-                torch.cuda.empty_cache()
+                get_accelerator().empty_cache()
 
             if mpu.is_pipeline_last_stage(ignore_virtual=True):
                 # Reduce across processes.
